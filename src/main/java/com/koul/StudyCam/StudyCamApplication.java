18343aeaba23714d70ebb8c220389c23717da2c4
Koul <dongik0122@gmail.com>
2025-10-02 23:49:27 +0900

    [#1] chore: Set Project
    - initialize Spring Boot project with Lombok and Spring Web Starter

---
+++ src/main/java/com/koul/StudyCam/StudyCamApplication.java
@@ -0,0 +1,13 @@
+package com.koul.StudyCam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyCamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koul/StudyCam/StudyCamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koul.StudyCam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
