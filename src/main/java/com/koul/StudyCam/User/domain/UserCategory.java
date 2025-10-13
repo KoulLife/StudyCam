4c6252f48f2e950f084f5aac2daafbf44645c466
Koul <dongik0122@gmail.com>
2025-10-13 16:19:42 +0900

    [#6] feat: add UserCategory entity

---
+++ src/main/java/com/koul/StudyCam/User/domain/UserCategory.java
@@ -0,0 +1,43 @@
+package com.koul.StudyCam.user.domain;
+
+import com.koul.StudyCam.category.domain.Category;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user_category",
+	uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "category_id"}))
+@Entity
+public class UserCategory {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "category_id", nullable = false)
+	private Category category;
+
+	@Builder
+	public UserCategory(User user, Category category) {
+		this.user = user;
+		this.category = category;
+	}
+}
